080e2c59fa135d23bbcb1e5b66838b6a22ce3e0a
BALAMURUGAN SOUPPOURAYEN <balamurugan.douxure@gmail.com>
2017-03-19 21:51:45 +0530

    Updated Organization name

--- ZopAppUITests/ZopAppUITests.m
+++ ZopAppUITests/ZopAppUITests.m
@@ -2,9 +2,10 @@
 //  ZopAppUITests.m
 //  ZopAppUITests
 //
-//  Created by BALAMURUGAN SOUPPOURAYEN on 30/03/16.
-//  Copyright © 2016 Clementek Private Limited. All rights reserved.
+// Copyright (C) 2017, puremvc-ios-standrad-framework, Balamurugan Souppourayen
 //
+// This software may be modified and distributed under the terms
+// of the MIT license.  See the LICENSE file for details.
 
 #import <XCTest/XCTest.h>
 
